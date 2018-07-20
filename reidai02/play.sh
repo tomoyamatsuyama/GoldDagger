1a4a0a5d99105a048bb714c135d9a0b019a7530b
tomoyamatsuyama <m.tomoya0221@gmail.com>
2018-07-20 13:17:39 +0900

    Update

--- reidai02/play.sh
+++ reidai02/play.sh
@@ -4,5 +4,5 @@ javac Solver.java
 
 for i in `seq 1 10`; do 
   java Gold | head -n 1 | awk '{print $1}'
-done | awk '{a += $1 } END {print a / 10}'
+done | awk '{print $1; a += $1 } END {print a / 10}'
 
